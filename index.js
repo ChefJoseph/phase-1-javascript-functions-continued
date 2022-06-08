29806630d1f09220da2e3d70f5b3f7973105f201
Joseph Hirotsu <jhirotsu@gmail.com>
2022-06-08 00:38:16 -0400

    commit tree fdabc648675d6ba055299a0f01e3e5c631aa5cb9

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+// function saturdayFun(activity) {
+    // console.log(`This Saturday I want to + ${activity}')
+    // if (activity = 'rollerskate') {
+        // return 
+    // } else {
+        // activity = 'bathe my dog'
+        // return 
+    // }
+// }
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function(adj="special") {
+      return `You are ${flair }${adj}${flair}!`;
+    }
+  }
+/*
+// function wrapAdjective() {
+    // const flair = "*"
+    // return function() {
+        // const adj = "special"
+        // return function() {
+        // console.log(`You are ${flair}${adj}${flair}!`)
+    }
+}
+}*/
